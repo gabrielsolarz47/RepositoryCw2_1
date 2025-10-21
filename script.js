ffc1b3560945eb1c8457faa6aaf6168184fa11f7
gabrielsolarz47 <gabinek03@gmail.com>
2025-10-21 09:24:28 +0000

    Com2_1.1

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-(function () {
+(function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
+  const ol = document.createElement('ol');
+  answer.appendChild(ol);
 
-  example.addEventListener("click", function () {
+  example.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -14,15 +16,25 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
+    fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET' })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        data.forEach(element => {
+          let li = document.createElement('li');
+          li.innerHTML = element.title;
+          ol.appendChild(li);
+        })
+      }
+      )
+  })
+
+  cw2.addEventListener("click", function() {
     //TODO
   })
 
-  cw2.addEventListener("click", function () {
-    //TODO
-  })
-
-  cw3.addEventListener("click", function () {
+  cw3.addEventListener("click", function() {
     //TODO
   })
 
